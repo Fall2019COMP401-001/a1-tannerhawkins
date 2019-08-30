238d0cb2413a95f8af52fe95aea9b7dcdc3f271d
tannerhawkins <54511510+tannerhawkins@users.noreply.github.com>
2019-08-30 12:43:37 -0400

    Update A1Novice.java

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,27 +10,23 @@ public class A1Novice {
 
 		// Your code follows here.
 		int customercount = scan.nextInt();
-		String[] customers = new String[customercount];
-		scan.nextLine();
 		for(int i=customercount; Math.abs(i)>0.1; i--) {
-			String name = scan.nextLine();
-			String[] temp = name.split(" ");
-			customers[i-1] = temp[0] + " " + temp[1];
-			int itemcount = Integer.parseInt(temp[2]);
+			String name = scan.next();
+			String lastname = scan.next();
+			int itemcount = scan.nextInt();
 			float total = 0;
 			for(int x=itemcount; Math.abs(x) > 0.1; x--) {
-				String itemline = scan.nextLine();
-				String[] temp2 = itemline.split(" ");
-				int numberofitem = Integer.parseInt(temp2[0]);
-				for (int y=numberofitem; Math.abs(y)>0.1; y--) {
-					float itemprice = Float.parseFloat(temp2[2]);
+				int itemnumber = scan.nextInt();
+				String itemname = scan.next();
+				float itemprice = scan.nextFloat();
+				for (int y=itemnumber; Math.abs(y)>0.1; y--) {
 					total += itemprice;
 				}
 			}
-			System.out.printf(temp[0].charAt(0) + ". " + temp[1] + ": " + "%.2f", total);
+			System.out.printf(name.charAt(0) + ". " + lastname + ": " + "%.2f", total);
 			System.out.println();
 		}
 		scan.close();
-		
+
 	}
 }
