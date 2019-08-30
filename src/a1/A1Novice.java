3a7a0302d2367024a706ec51d61b027e72461c59
tannerhawkins <54511510+tannerhawkins@users.noreply.github.com>
2019-08-30 12:20:36 -0400

    Update A1Novice.java

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,28 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int customercount = scan.nextInt();
+		String[] customers = new String[customercount];
+		scan.nextLine();
+		for(int i=customercount; Math.abs(i)>0.1; i--) {
+			String name = scan.nextLine();
+			String[] temp = name.split(" ");
+			customers[i-1] = temp[0] + " " + temp[1];
+			int itemcount = Integer.parseInt(temp[2]);
+			float total = 0;
+			for(int x=itemcount; Math.abs(x) > 0.1; x--) {
+				String itemline = scan.nextLine();
+				String[] temp2 = itemline.split(" ");
+				int numberofitem = Integer.parseInt(temp2[0]);
+				for (int y=numberofitem; Math.abs(y)>0.1; y--) {
+					float itemprice = Float.parseFloat(temp2[2]);
+					total += itemprice;
+				}
+			}
+			System.out.printf(temp[0].charAt(0) + ". " + temp[1] + ": " + "%.2f", total);
+			System.out.println();
+		}
+		scan.close();
 		
 	}
 }
